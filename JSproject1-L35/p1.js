039ce91ca467ee74c94732d7cf5af7183f419ce3
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2024-12-31 01:46:29 +0530

    Style added

---
+++ JSproject1-L35/p1.js
@@ -0,0 +1,30 @@
+const buttons=document.querySelectorAll('.button')
+// console.log(buttons)
+const body=document.querySelector('body')
+buttons.forEach(function(button){
+    // console.log(button)
+    button.addEventListener('click',function(e){
+        console.log(e)
+        // console.log(e.target.id)
+        switch (e.target.id) {
+            case 'red':
+                body.style.backgroundColor=e.target.id;
+                break;
+            case 'blue':
+                body.style.backgroundColor=e.target.id;
+                break;
+            case 'yellow':
+                body.style.backgroundColor=e.target.id;
+                break;
+            case 'aqua':
+                body.style.backgroundColor=e.target.id;
+                break;
+            default:
+                body.style.backgroundColor='aliceblue';
+                break;
+        }
+    })
+});
+// document.querySelector('.defaultbtn').addEventListener('click',function(e){
+//     document.body.style.backgroundColor=e.target.className;
+// });
