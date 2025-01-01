1181f5b8c1eabafcd951756c683505afce2efef0
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-01-02 00:11:04 +0530

    Color Changer Style added

---
+++ JSproject1-L35/p1.js
@@ -0,0 +1,24 @@
+const buttons=document.querySelectorAll('.button')
+const body=document.querySelector('body')
+buttons.forEach(function(button){
+    button.addEventListener('click',function(e){
+        console.log(e)
+        switch (e.target.id) {
+            case 'red':
+                body.style.backgroundColor=e.target.id;
+                break;
+            case 'blue':
+                body.style.backgroundColor=e.target.id;
+                break;
+            case 'yellow':
+                body.style.backgroundColor=e.target.id;
+                break;
+            case 'aqua':
+                body.style.backgroundColor=e.target.id;
+                break;
+            default:
+                body.style.backgroundColor='aliceblue';
+                break;
+        }
+    })
+});
