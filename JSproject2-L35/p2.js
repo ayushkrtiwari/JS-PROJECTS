fb6d7017a413393d968dd66c16328d358c2b907d
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-01-30 00:41:06 +0530

    Result Calculated

--- JSproject2-L35/p2.js
+++ JSproject2-L35/p2.js
@@ -6,5 +6,10 @@ form.addEventListener('submit',function(e){
     e.preventDefault()
     const height=parseInt(document.querySelector('#height').value)
     const weight=parseInt(document.querySelector('#weight').value)
-    
+    const results=document.querySelector('#results')
+    {
+        const bmi=(weight/((height*height)/10000)).toFixed(2)
+        //show the result
+        results.innerHTML=`<span>${bmi}</span>`
+    }
 });
